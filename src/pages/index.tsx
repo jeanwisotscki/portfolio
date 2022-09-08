af779abd01f50fc4adebbf62a5925d8612fc9eed
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-09-08 09:53:08 -0300

    organização de arquivos

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+import { Title } from "./styles";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Title>olá</Title>
+    </div>
+  );
+};
+
+export default Home;
