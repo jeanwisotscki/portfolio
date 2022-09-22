7f993f2e35eb5236d4c3227951fd785a42bbd831
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-09-22 11:45:52 -0300

    refactor: estilizando página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,38 @@ import type { NextPage } from "next";
 
 import { LayoutBase } from "../components/LayoutBase";
 
-import { Container, Stacks, DescriptionWrapper, TitleWrapper } from "./styles";
+import {
+  Container,
+  Stacks,
+  DescriptionWrapper,
+  Header,
+  Presentation,
+} from "./styles";
 
 const Home: NextPage = () => {
   return (
     <LayoutBase>
       <Container>
-        <TitleWrapper>
-          <span>Olá 👋</span>
-          <div>
-            <span>eu sou </span>
-            <strong>Jean Wisotscki</strong>
-          </div>
-        </TitleWrapper>
-        <Stacks>
-          React | Next.js | TypeScript | JavaScript | Styled-components |
-          SASS/SCSS
-        </Stacks>
+        <Header>
+          <Presentation>
+            <span>Olá 👋</span>
+            <div>
+              <span>eu sou </span>
+              <strong>Jean Wisotscki</strong>
+            </div>
+          </Presentation>
+          <Stacks>
+            React | Next.js | TypeScript | JavaScript | Styled-components |
+            SASS/SCSS
+          </Stacks>
+        </Header>
         <DescriptionWrapper>
           <p>
             Desenvolvedor front-end em formação, estudante de Análise e
             Desenvolvimento de Sistemas e apaixonado por tecnologia.
           </p>
         </DescriptionWrapper>
+        <p>Leia mais sobre mim</p>
       </Container>
     </LayoutBase>
   );
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -3,20 +3,19 @@ import styled, { keyframes } from "styled-components";
 export const Container = styled.div`
   height: 75vh;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   border: 1px solid red;
+`;
+
+export const Header = styled.div`
+  padding: 1rem;
 
   @media (max-width: 769px) {
-    justify-content: start;
-    flex-direction: column;
+    margin-top: 1rem;
   }
 `;
 
-export const TitleWrapper = styled.div`
-  max-width: 50%;
-  width: 50%;
-
+export const Presentation = styled.div`
   span {
     color: ${(props) => props.theme.colors.secondary};
     font-size: 1.5rem;
@@ -34,30 +33,35 @@ export const TitleWrapper = styled.div`
 
     strong {
       color: ${(props) => props.theme.colors.primary};
-      font-size: 3rem;
+      font-size: 5rem;
       line-height: 0;
-    }
-  }
 
-  @media (max-width: 769px) {
-    margin-top: 3rem;
-    min-width: 100%;
+      @media (max-width: 769px) {
+        font-size: 3rem;
+      }
+    }
   }
 `;
 
 export const Stacks = styled.strong`
   color: ${(props) => props.theme.colors.secondary};
-  margin-top: 0.5rem;
+  display: block;
+  margin: 0.5rem 0;
+  text-align: center;
   opacity: 0.4;
 `;
 
 export const DescriptionWrapper = styled.div`
-  max-width: 50%;
-  width: 50%;
   color: ${(props) => props.theme.colors.primary};
+  padding: 1rem;
+  opacity: 0.7;
+
+  p {
+    font-size: 1.5rem;
+    text-align: justify;
+  }
 
   @media (max-width: 769px) {
-    min-width: 100%;
     margin-top: 2rem;
   }
 `;
