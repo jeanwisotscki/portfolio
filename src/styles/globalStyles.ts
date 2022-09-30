c20a5e93a4fa14cd9944bf36f7adc9cbc6f201ce
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-09-30 08:57:25 -0300

    styled: scrollbar personalizada

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -13,6 +13,25 @@ export const GlobalStyle = createGlobalStyle`
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
+  body {
+  scrollbar-width: thin;
+  scrollbar-color: #333 #aaa;
+  }
+
+  body::-webkit-scrollbar {
+    width: 3px;
+    height: 3px; /* A altura só é vista quando a rolagem é horizontal */
+  }
+
+  body::-webkit-scrollbar-track {
+    background: #aaa;
+    padding: 2px;
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: #333;
+  }
+
   a {
     color: inherit;
     text-decoration: none;
